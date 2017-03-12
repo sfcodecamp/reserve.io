1f08b65c21fbff777fbfe75b4b906b5d9994a04a
Tiffany Lam <tlam5@sbcglobal.net>
2017-03-11 17:10:34 -0800

    Make Material-UI cards

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,26 @@
 import React, { Component } from 'react';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-
+import RestaurantCard from './card';
 import './App.css';
+
+const styles = {
+  display: 'flex',
+  flexWrap: 'wrap',
+  justifyContent: 'space-around'
+}
 
 class App extends Component {
   render() {
     return (
-      <h1>hello</h1>
+      <MuiThemeProvider>
+        <div style={styles}>
+          <h1>Welcome to Reserve!</h1>
+          <RestaurantCard />
+          <RestaurantCard />
+          <RestaurantCard />
+          <RestaurantCard />
+        </div>
+      </MuiThemeProvider>
     );
   }
 }
