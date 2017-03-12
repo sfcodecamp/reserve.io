745b5e5186fd016fef93355a2587779296aa8be6
Tiffany Lam <tlam5@sbcglobal.net>
2017-03-11 17:51:04 -0800

    Resolve conflict with new Form

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,26 @@
 import React, { Component } from 'react';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-
+import RestaurantCard from './card';
 import './App.css';
 
 import Form from '../containers/form'
+
+const styles = {
+  display: 'flex',
+  flexWrap: 'wrap',
+  justifyContent: 'space-around'
+}
 
 class App extends Component {
   render() {
     return (
       <MuiThemeProvider>
-        <div>
+        <div style={styles}>
           <Form />
+          <RestaurantCard />
+          <RestaurantCard />
+          <RestaurantCard />
+          <RestaurantCard />
         </div>
       </MuiThemeProvider>
     );
