51dfc98777cb529a5389a1e6d7febb4ce17972cb
Sean Smith <sean.smith.2009@gmail.com>
2017-03-12 14:08:22 -0700

    serverless api endpoint for availability

--- serverless/handler.js
+++ serverless/handler.js
@@ -33,29 +33,44 @@ module.exports.opentable = (event, context, callback) => {
 
 };
 
-module.exports.landingPage = (event, context, callback) => {
-  let dynamicHtml = '<p>Hey Unknown!</p>';
-  // check for GET params and use if available
-  if (event.queryStringParameters && event.queryStringParameters.name) {
-    dynamicHtml = `<p>Hey ${event.queryStringParameters.name}!</p>`;
+module.exports.info = (event, context, callback) => {
+
+  let id = (event.queryStringParameters && event.queryStringParameters.id) ? event.queryStringParameters.id : null;
+  let size = (event.queryStringParameters && event.queryStringParameters.size) ? event.queryStringParameters.size : '2';
+  let start_time = (event.queryStringParameters && event.queryStringParameters.time) ? event.queryStringParameters.time : '2017-03-29T18%3A00';
+
+  var response = {};
+
+  if (id) {
+    const url = `https://platform.otqa.com/availability/${id}?start_date_time=${start_time}&party_size=${size}&forward_minutes=120&backward_minutes=30`;
+    axios.get(url, {
+      method: 'get',
+      headers: {'Authorization': 'Bearer 27037c67-f394-4cfd-ab51-069ac71132fb'},
+    }).then(result => {
+      response = {
+        statusCode: 200,
+        body: JSON.stringify({
+          message: result.data
+        })
+      };
+      callback(null, response);
+    }).catch(error => {
+      response = {
+        statusCode: 200,
+        body: JSON.stringify({
+          error: 'There was a problem retrieving the results from OpenTable...'
+        })
+      };
+      callback(null, response);
+    });
+  } else {
+    response = {
+      statusCode: 200,
+      body: JSON.stringify({
+        error: `No ID was provided...`
+      })
+    };
+    callback(null, response);
   }
 
-  const html = `
-    <html>
-      <body>
-        <h1>Landing Page</h1>
-        ${dynamicHtml}
-      </body>
-    </html>`;
-
-  const response = {
-    statusCode: 200,
-    headers: {
-      'Content-Type': 'text/html',
-    },
-    body: html,
-  };
-
-  // callback is sending HTML back
-  callback(null, response);
 };
