e18a81deb3241c72abcc0344d94ad9d589a4c4f6
Kenzo Mendoza <kenzom954@gmail.com>
2017-03-12 09:55:04 -0700

    fixed CORS issue

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,9 +1,15 @@
 import axios from 'axios';
 
+// This is for CORS issue
+// var config = {
+//   headers: {'Access-Control-Allow-Headers': 'Origin'}
+// };
+
 import { GET_RESTAURANT } from './types';
 
 export function sendForm(params){
-  const request = axios.get(`https://hu1b1nudk7.execute-api.us-east-1.amazonaws.com/dev/opentable?city=${params.city}`);
+  const url = `https://hu1b1nudk7.execute-api.us-east-1.amazonaws.com/dev/opentable?city=${params.city}`
+  const request = axios.get(url);
   return (dispatch) => {
     request.then( ({data}) => {
       dispatch({type: GET_RESTAURANT, payload: data})
