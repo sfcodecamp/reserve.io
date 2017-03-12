523bdd59db7c0f6943dc12ac8bdad249f19256fa
Tiffany Lam <tlam5@sbcglobal.net>
2017-03-11 17:14:50 -0800

    Resolve merge conflict with newly added Form and package

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,27 @@
 import React, { Component } from 'react';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-
+import RestaurantCard from './card';
 import './App.css';
 
 import Form from '../containers/form'
+=======
+const styles = {
+  display: 'flex',
+  flexWrap: 'wrap',
+  justifyContent: 'space-around'
+}
 
 class App extends Component {
   render() {
     return (
       <MuiThemeProvider>
-        <div>
+        <div style={styles}>
+          <h1>Welcome to Reserve!</h1>
           <Form />
+          <RestaurantCard />
+          <RestaurantCard />
+          <RestaurantCard />
+          <RestaurantCard />
         </div>
       </MuiThemeProvider>
     );
